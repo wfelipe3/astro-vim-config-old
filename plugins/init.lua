74b72d645c270e302915e5270fd5263836e640a4
William Rojas <willia.rojas@externals.adidas.com>
2023-03-24 10:23:23 -0500

    config

---
+++ plugins/init.lua
@@ -0,0 +1,16 @@
+return {
+  ['Olical/conjure'] = {},
+  ["Pocco81/auto-save.nvim"] = {},
+  ["EdenEast/nightfox.nvim"] = {},
+  ["scalameta/nvim-metals"] = {
+    config = function() require "user.plugins.scala" end,
+  },
+  ["justinmk/vim-sneak"] = {},
+  ["tpope/vim-surround"] = {},
+  ["vim-scripts/argtextobj.vim"] = {},
+  ["chaoren/vim-wordmotion"] = {},
+  ["michaeljsmith/vim-indent-object"] = {},
+  ["itchyny/vim-cursorword"] = {},
+  ["neovim/nvim-lspconfig"] = {},
+  ["kabouzeid/nvim-lspinstall"] = {},
+}
